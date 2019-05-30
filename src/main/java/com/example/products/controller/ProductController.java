94af841e21273605b7b65a6f16e34b00a92999c5
Venkata Thanmai Mande <vm6710@rit.edu>
2019-05-29 23:28:01 -0400

    created skeleton classes

---
+++ src/main/java/com/example/products/controller/ProductController.java
@@ -0,0 +1,8 @@
+package com.example.products.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+
+}
---
+++ src/main/java/com/example/products/domain/Product.java
@@ -0,0 +1,10 @@
+package com.example.products.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Product implements Serializable {
+
+}
